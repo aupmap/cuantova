22016bb8b6954d1200bee2592320578be2e1a1ac
au <aupmap@gmail.com>
2021-07-03 17:40:11 -0500

    settings DEBUG = True

--- Cuantovaser/settings.py
+++ Cuantovaser/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*oiy&o+q+5#28-^+y_3gmyc!bhaynelr#mygt0of=2f@hokm91'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
